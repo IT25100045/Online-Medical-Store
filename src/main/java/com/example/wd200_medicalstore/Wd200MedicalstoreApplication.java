2f7f1302482dd20f6fd2d32c6daeb5129f4f309b
IT25103306 <it25103306@my.sliit.lk>
2026-05-15 16:23:33 +0530

    Initial project structure (Spring Boot backend setup)

---
+++ src/main/java/com/example/wd200_medicalstore/Wd200MedicalstoreApplication.java
@@ -0,0 +1,13 @@
+package com.example.wd200_medicalstore;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Wd200MedicalstoreApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(Wd200MedicalstoreApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/example/wd200_medicalstore/Wd200MedicalstoreApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.wd200_medicalstore;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class Wd200MedicalstoreApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
